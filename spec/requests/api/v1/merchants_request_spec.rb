e94cb40b40855ca881147db3c2d96fc48502b034
Thomas Casady <tscasady@gmail.com>
2023-02-06 16:03:14 -0600

    Edit test data to reflect serializer structure.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,29 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant API' do
+
   before :each do
     create_list(:merchant, 5)
   end
+
   it 'can return all merchants' do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(merchants.count).to eq(5)
 
     merchants.each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an Integer
+      expect(merchant[:id]).to be_an String
 
       expect(merchant).to have_key(:type)
-      expect(merchant[:title]).to be_a(String)
+      expect(merchant[:type]).to be_a(String)
 
-      expect(merchant).to have_key(:attributes)
-      expect(merchant[:author]).to be_a(Hash)
-
-      expect(merchant[:attributes][:name]).to have_key(:name)
+      expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
@@ -35,18 +34,18 @@ RSpec.describe 'Merchant API' do
 
     expect(response).to be_successful
 
-    merchant = JSON.parse(response.body, symbolize_names: true)
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(merchant).to have_key(:id)
-    expect(merchant[:id]).to be_an Integer
+    expect(merchant[:id]).to be_an String
 
     expect(merchant).to have_key(:type)
-    expect(merchant[:title]).to be_a(String)
+    expect(merchant[:type]).to be_a(String)
 
     expect(merchant).to have_key(:attributes)
-    expect(merchant[:author]).to be_a(Hash)
+    expect(merchant[:attributes]).to be_a(Hash)
 
-    expect(merchant[:attributes][:name]).to have_key(:name)
+    expect(merchant[:attributes]).to have_key(:name)
     expect(merchant[:attributes][:name]).to be_a(String)
   end
 end

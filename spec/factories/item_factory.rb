6066d8f48c2e3ac185abf8e9160e71e4a81923b0
Thomas Casady <tscasady@gmail.com>
2023-02-06 17:20:55 -0600

    Add all items and specific item api request test.

--- spec/factories/item_factory.rb
+++ spec/factories/item_factory.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::Name.name}
     description { Faker::Lorem.paragraph}
     unit_price { Faker::Commerce.price }
-    merchant { Faker::Number.number(digits: 2)}
+    merchant { create(:merchant) }
   end
 end

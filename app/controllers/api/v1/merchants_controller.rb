8b1159445e93ad90554332513d6848dbf8560243
Thomas Casady <tscasady@gmail.com>
2023-02-06 15:49:02 -0600

    Add index and show methods to merchant controller.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,11 +2,11 @@ module Api
   module V1
     class MerchantsController < ApplicationController
       def index
-
+        render json: Merchant.all
       end
 
       def show
-
+        render json: Merchant.find(params[:id])
       end
     end
   end

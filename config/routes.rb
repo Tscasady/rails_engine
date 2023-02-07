cd614af362b0839d02fe32e300afb6c72f757c2d
Thomas Casady <tscasady@gmail.com>
2023-02-07 16:28:04 -0600

    Add routes for search and search all for merchants.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
+
   namespace :api do
     namespace :v1 do
       resources :merchants do
+        scope module: 'merchants' do
+          collection do
+            get 'find', to: 'search#search'
+            get 'find_all', to: 'search#search_all'
+          end
+        end
         resources :items, only: [:index], controller: 'merchants/items'
       end
 
@@ -12,6 +19,7 @@ Rails.application.routes.draw do
           get 'merchant', to: 'items/merchants#show'
         end
       end
+
     end
   end
 end
